9a53d02b79b33711786084fdbdfda419aec6108f
RayGar7 <rgarcia.7770@gmail.com>
2025-04-12 15:10:03 -0500

    Built AppNavigator

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
-import * as Animatable from 'react-native-animatable';
 
 import Screen from '../components/Screen';
 import Card from '../components/Card';
-import AppText from '../components/AppText';
-import AppButton from '../components/AppButton';
-import ActivityIndicator from '../components/ActivityIndicator';
-
-import listingsApi from '../api/listings';
-import useLocation from '../hooks/useLocation';
-import useApi from "../hooks/useApi";
-
-import routes from '../navigation/routes';
 
 import colors from '../config/colors';
+
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in great condition",
+    price: 1000,
+    image: require("../assets/couch.jpg"),
+  },
+];
 
 function ListingsScreen(props) {
     return (

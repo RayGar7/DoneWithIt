986cfdc4461a8f066ebfc85818ea45251bd7f9c9
RayGar7 <rgarcia.7770@gmail.com>
2025-04-28 13:50:00 -0500

    Fixed the logout issue as well as all the broken pages that were resulting from the BottomTabs library which was deprecated and causing all of those issues

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
@@ -15,65 +15,6 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import OfflineNotice from './app/components/OfflineNotice';
 import AuthContext from './app/auth/context';
 
-const Link = () => {
-  const navigation = useNavigation();
-  const [user, setUser] = useState();
-  return (
-    <Screen>
-      <Text>Tweets</Text>
-      <AppButton 
-        title="View Tweet"
-        onPress={() => navigation.navigate("TweetDetails", { id: 1})} 
-      />
-    </Screen>
-  )
-}
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <AppButton 
-      title="View Tweet" onPress={() => navigation.push("Tweets")}
-    />
-  </Screen>
-);
-
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details</Text>
-    <Text>{route.params.id}</Text>
-  </Screen>
-)
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen 
-      name="TweetDetails" 
-      component={TweetDetails} 
-      options={({ route }) => ({ title: route.params.id })} 
-    />
-  </Stack.Navigator>
-);
-
-const Account = () => <Screen><Text>Account</Text></Screen>
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen 
-      name="Feed" 
-      component={Tweets} 
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen name="Account" component={Account} />
-  </Tab.Navigator>
-);
 
 export default function App() {
   const [user, setUser] = useState();
@@ -82,9 +23,22 @@ export default function App() {
       <GestureHandlerRootView style={{ flex: 1 }}>
         <OfflineNotice />
         <NavigationContainer theme={navigationTheme}>
-          {user ? <AppNavigator /> : <AuthNavigator />}
+          {user ? (
+            <AppNavigator />
+          ) : (
+            <AuthNavigator />
+          )}
         </NavigationContainer>
       </GestureHandlerRootView>
     </AuthContext.Provider>
   );
 }
+
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    justifyContent: 'center',
+    aligntItems: 'center'
+  },
+});

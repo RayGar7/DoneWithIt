b470824009704956ff36a7e8a39bc4d670d56ebf
RayGar7 <rgarcia.7770@gmail.com>
2025-05-01 13:39:14 -0500

    Created a hook for authentication

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,13 +1,10 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Image } from 'react-native';
 
 import Screen from '../components/Screen';
 import { ErrorMessage, Form, FormField, SubmitButton } from "../components/forms";
 import * as Yup from 'yup';
 import authApi from '../api/auth';
-import { jwtDecode } from 'jwt-decode';
-import AuthContext from '../auth/context';
-import authStorage from "../auth/storage";
 
 
 const validationSchema = Yup.object().shape({
@@ -17,21 +14,17 @@ const validationSchema = Yup.object().shape({
 
 
 function LoginScreen(props) {
-    const authContext = useContext(AuthContext);
+    const auth = useAuth();
     const [loginFailed, setLoginFailed] = useState(false);
 
 
 
     const handleSubmit = async ({ email, password}) => {
         const result = await authApi.login(email, password);
-        if (!result.ok) {
-            return setLoginFailed(true);
-        }
+        if (!result.ok) return setLoginFailed(true);
         setLoginFailed(false);
-        const user = jwtDecode(result.data);
-        authContext.setUser(user);
-        authStorage.storeToken(result.data);
-    }
+        auth.logIn(result.data);
+    };
 
     return (
         <Screen style={styles.container}>
